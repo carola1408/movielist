d13ea9668b6157e4922a19caee3e0024f6ed6941
user <carola1408@yahoo.com.tw>
2022-11-27 22:13:48 +0800

    variable declaration index & poster url

---
+++ index.js
@@ -0,0 +1,4 @@
+// 先將會使用到的網址(index & poster url) 宣告變數(variable declaration)
+const BASE_URL = "https://movie-list.alphacamp.io";
+const INDEX_URL = BASE_URL + "/api/v1/movies/";
+const POSTER_URL = BASE_URL + "/posters/";
